625a2ce95d6a5985018c2ddfeccded96ec0046ab
osmanforhad <osmanforhad93@gmail.com>
2025-07-09 16:34:40 +0600

    working with function return type

---
+++ functionReturnType.js
@@ -0,0 +1,19 @@
+//function return type, in js we dont need to define return type
+function myFunction() {
+    return "Bangladesh";
+}
+//function calling
+const result = myFunction();
+console.log(result);
+
+
+
+function functionOne() {
+    return 20;
+}
+
+function functionTwo() {
+    return 30;
+}
+//calling function
+console.log(functionOne() + functionTwo());
