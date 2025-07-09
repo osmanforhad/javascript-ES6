0042950521f2bef80c7c295ca9a1d316c4809ef5
osmanforhad <osmanforhad93@gmail.com>
2025-07-09 18:11:36 +0600

    working with multidymentional array

---
+++ ES6MultiDymentionalArray.js
@@ -0,0 +1,11 @@
+//Multidymentional Array
+
+//Singale Array
+var bangladesh = ["Dhaka", "Rangpur", "Chittagong"];
+var india = ["Mumbai", "Channai", "Kolkata"];
+var china = ["Uhan", "Shanhai", "Hongkong"];
+//Multidymentional Array
+var asia = [bangladesh, india, china];
+//extract data from Multidymentional array
+const result = asia[1][2];//kolkata
+console.log(result);//kolkata
