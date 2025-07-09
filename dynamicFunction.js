57c11a929e2c8583e81456d3a43c33f60505ec0d
osmanforhad <osmanforhad93@gmail.com>
2025-07-09 12:28:32 +0600

    working with dynamic function

---
+++ dynamicFunction.js
@@ -0,0 +1,7 @@
+//Dynamic Function, function without name but store into a variable then we call it as a dynamic function
+var f = function (name) {
+    return name;
+}
+//calling variable as like function calling
+const result = f("osman forhad");
+console.log(result);
