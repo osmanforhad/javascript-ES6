6cecc72695683c71d5505a8c52e260d96f329db7
osmanforhad <osmanforhad93@gmail.com>
2025-07-09 16:50:10 +0600

    working with arrow function

---
+++ functionArrow.js
@@ -0,0 +1,7 @@
+//Arrow function () => {} which also called anonymouse function 
+var myFunction = (x) => {
+    console.log(x);
+}
+//function calling
+let result = myFunction("My first arrow function");
+console.log(result);
